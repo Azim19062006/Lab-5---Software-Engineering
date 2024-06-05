81de977381a37123f99645b7e35ac3ebf107987c
VICTUS <VICTUS@LAPTOP-O7RUJLVH>
2024-06-05 20:42:35 +0600

    Add project files.

---
+++ lab5/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Button btn_select;
+        private TextBox txt_path;
+        private ListBox lst_of_folder;
+        private DataGridView dataGridViewFiles;
+        private Button btn_process;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem about_inf;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region
+
+        private void InitializeComponent()
+        {
+            btn_select = new Button();
+            txt_path = new TextBox();
+            lst_of_folder = new ListBox();
+            dataGridViewFiles = new DataGridView();
+            btn_process = new Button();
+            menuStrip = new MenuStrip();
+            about_inf = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewFiles).BeginInit();
+            menuStrip.SuspendLayout();
+            SuspendLayout();
+
+
+            btn_select.Location = new Point(1150, 75);
+            btn_select.Name = "btn_select";
+            btn_select.Size = new Size(250, 75);
+            btn_select.TabIndex = 0;
+            btn_select.Text = "Select";
+            btn_select.UseVisualStyleBackColor = true;
+
+
+            txt_path.BackColor = Color.LightGray;
+            txt_path.Location = new Point(12, 80);
+            txt_path.Name = "txt_path";
+            txt_path.ReadOnly = true;
+            txt_path.Size = new Size(1090, 27);
+            txt_path.TabIndex = 1;
+
+
+            lst_of_folder.FormattingEnabled = true;
+            lst_of_folder.Location = new Point(12, 120);
+            lst_of_folder.Name = "lst_of_folder";
+            lst_of_folder.Size = new Size(330, 450);
+            lst_of_folder.TabIndex = 2;
+
+
+            dataGridViewFiles.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewFiles.Location = new Point(350, 120);
+            dataGridViewFiles.Name = "dataGridViewFiles";
+            dataGridViewFiles.RowHeadersWidth = 51;
+            dataGridViewFiles.Size = new Size(750, 450);
+            dataGridViewFiles.TabIndex = 3;
+
+
+            btn_process.Location = new Point(922, 580);
+            btn_process.Name = "btn_process";
+            btn_process.Size = new Size(450, 90);
+            btn_process.TabIndex = 4;
+            btn_process.Text = "Process";
+            btn_process.UseVisualStyleBackColor = true;
+            btn_process.Visible = false;
+
+
+            menuStrip.ImageScalingSize = new Size(20, 20);
+            menuStrip.Items.AddRange(new ToolStripItem[] { about_inf });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(800, 28);
+            menuStrip.TabIndex = 0;
+            menuStrip.Text = "menuStrip";
+
+
+            about_inf.BackColor = Color.LightBlue;
+            about_inf.Name = "about_inf";
+            about_inf.Size = new Size(64, 24);
+            about_inf.Text = "About";
+            about_inf.Click += Click_On_AboutMenuItem;
+
+
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 500);
+            Controls.Add(btn_select);
+            Controls.Add(txt_path);
+            Controls.Add(lst_of_folder);
+            Controls.Add(dataGridViewFiles);
+            Controls.Add(btn_process);
+            Controls.Add(menuStrip);
+            MainMenuStrip = menuStrip;
+            Name = "Form1";
+            Text = "MyFirstForm";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewFiles).EndInit();
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ lab5/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Size = new Size((int)(Screen.PrimaryScreen.WorkingArea.Width * 0.8), (int)(Screen.PrimaryScreen.WorkingArea.Height * 0.8));
+
+            btn_select.Click += Click_On_BtnSelectFolder;
+            lst_of_folder.DoubleClick += DoubleClick;
+            dataGridViewFiles.CellDoubleClick += DataGridViewFiles_CellDoubleClick;
+            btn_process.Click += Click_On_BtnProcessFiles;
+        }
+
+        private void Click_On_AboutMenuItem(object sender, EventArgs e)
+        {
+            MessageBox.Show("Developer: Mamatbaev Azim\nE-mail: mamatbaev_a@auca.kg\nGitHub: https://github.com", "Information about Developer");
+        }
+
+        private void Click_On_BtnSelectFolder(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txt_path.Text = folderBrowserDialog.SelectedPath;
+                    LoadFolder(folderBrowserDialog.SelectedPath);
+                    btn_process.Visible = true;
+                }
+            }
+        }
+
+        private void LoadFolder(string folderPath)
+        {
+            lst_of_folder.Items.Clear();
+            dataGridViewFiles.Rows.Clear();
+            dataGridViewFiles.Columns.Clear();
+
+            dataGridViewFiles.Columns.Add("Name", "Name");
+            dataGridViewFiles.Columns.Add("LastModified", "Last Modified");
+            dataGridViewFiles.Columns.Add("Size", "Size (bytes)");
+            dataGridViewFiles.Columns.Add("ProcessingTime", "Processing Time");
+
+            try
+            {
+                string[] directories = Directory.GetDirectories(folderPath);
+                foreach (var directory in directories)
+                {
+                    lst_of_folder.Items.Add(Path.GetFileName(directory));
+                }
+
+                string[] files = Directory.GetFiles(folderPath);
+                foreach (var file in files)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    dataGridViewFiles.Rows.Add(fileInfo.Name, fileInfo.LastWriteTime, fileInfo.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading folder contents: " + ex.Message);
+            }
+        }
+
+        private void DoubleClick(object sender, EventArgs e)
+        {
+            if (lst_of_folder.SelectedItem != null)
+            {
+                string selectedFolderName = lst_of_folder.SelectedItem.ToString();
+                string selectedFolderPath = Path.Combine(txt_path.Text, selectedFolderName);
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(selectedFolderPath);
+
+                Form folderInfoForm = new Form
+                {
+                    Text = "Folder Info",
+                    Size = new Size(300, 200)
+                };
+
+                Label lblFolderName = new Label
+                {
+                    Text = "Name: " + directoryInfo.Name,
+                    AutoSize = true,
+                    Location = new Point(10, 10)
+                };
+
+                Label lblLastModified = new Label
+                {
+                    Text = "Last Modified: " + directoryInfo.LastWriteTime.ToString(),
+                    AutoSize = true,
+                    Location = new Point(10, 40)
+                };
+
+                folderInfoForm.Controls.Add(lblFolderName);
+                folderInfoForm.Controls.Add(lblLastModified);
+
+                folderInfoForm.ShowDialog();
+            }
+        }
+
+        private void DataGridViewFiles_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dataGridViewFiles.Rows.Count)
+            {
+                var row = dataGridViewFiles.Rows[e.RowIndex];
+                string fileName = row.Cells["Name"].Value.ToString();
+                string filePath = Path.Combine(txt_path.Text, fileName);
+
+                var result = MessageBox.Show($"Хотите продублировать файл '{fileName}'?", "Дублировать", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        string newFilePath = Path.Combine(txt_path.Text, Path.GetFileNameWithoutExtension(fileName) + "_copy" + Path.GetExtension(fileName));
+                        File.Copy(filePath, newFilePath);
+                        LoadFolder(txt_path.Text);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private async void Click_On_BtnProcessFiles(object sender, EventArgs e)
+        {
+            var random = new Random();
+            var tasks = dataGridViewFiles.Rows.Cast<DataGridViewRow>().Select(async row =>
+            {
+                int delay = random.Next(1, dataGridViewFiles.Rows.Count + 1);
+                await Task.Delay(delay * 1000);
+                row.Cells["ProcessingTime"].Value = delay;
+            }).ToArray();
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
